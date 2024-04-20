20005c41a8be9c2cedfe168f90a28a2ed5dec991
PJCR24 <60559691@continental.edu.pe>
2024-04-20 13:15:42 -0500

    Carpeta CuevaRamosPatrickJavierSemana5 subida

---
+++ CuevaRamosPatrickJavierSemana5/CapaPresentacion/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaEntidad;
+
+namespace CapaPresentacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static Persona persona = new Persona();
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void TxtApellidos_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnRegistrar_Click(object sender, EventArgs e)
+        {
+            //Asignar valores a los atributos de la clase Persona
+            persona.Apellidos = txtApellidos.Text;
+            persona.Nombres = txtNombres.Text;
+            persona.Edad = Convert.ToInt32(txtEdad.Text);
+            MessageBox.Show("Se registro correctamente el objeto Persona");
+        }
+
+        private void BtnReporte_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(persona.DevolverDatos());
+        }
+
+        private void TabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CuevaRamosPatrickJavierSemana5/CapaPresentacionWeb/WebForm1.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="WebForm1.aspx.vb" Inherits="CapaPresentacionWeb.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Datos de Alumno</div>
+        <p>
+            Apellidos<asp:TextBox ID="txtApellidos" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Nombres<asp:TextBox ID="txtNombres" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Edad<asp:TextBox ID="txtEdad" runat="server"></asp:TextBox>
+        </p>
+        <asp:Button ID="btnRegistrar" runat="server" Text="Registrar" />
+        <asp:Button ID="btnReporte" runat="server" Text="Reporte" />
+        <br />
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="lblResultado" runat="server" Text="Label"></asp:Label>
+        <br />
+    </form>
+</body>
+</html>
